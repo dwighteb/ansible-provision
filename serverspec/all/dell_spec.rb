c08b362d6dd07c2811e48c8c34bb38489e9656fd
Dwight Barnette <dwighteb@gmail.com>
2016-01-10 18:14:00 -0500

    Added docker install to dell role

--- serverspec/all/dell_spec.rb
+++ serverspec/all/dell_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 packages = [
   'anacron',
   'apt-cacher-ng',
+  'docker-engine',
   'smartmontools',
   'squid-deb-proxy-client']
 
@@ -12,9 +13,11 @@ packages.each do |pkg|
   end
 end
 
-describe service('apt-cacher-ng') do
-  it { should be_enabled }
-  it { should be_running }
+['apt-cacher-ng','docker'].each do |services|
+  describe service(services) do
+    it { should be_enabled }
+    it { should be_running }
+  end
 end
 
 describe service('smartd'), :if => os[:release] == '15.10' do
@@ -27,6 +30,20 @@ describe service('smartmontools'), :if => os[:release] == '14.04' do
   it { should be_running }
 end
 
+describe file('/etc/default/grub') do
+  its(:content) { should match /GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/}
+end
+
+describe group('docker') do
+  it { should exist }
+end
+
 describe port('3142') do
   it { should be_listening.with('tcp') }
+end
+
+['dwighteb','ubuntu'].each do |username|
+  describe user(username) do
+    it { should belong_to_group 'docker' }
+  end
 end
