0856fad12f59516ba164d9eb398a1600f2652b1e
Dwight Barnette <dwighteb@gmail.com>
2017-02-22 19:44:35 -0500

    Added user ubuntu to group docker in dockercloud

--- serverspec/dockercloud_spec.rb
+++ serverspec/dockercloud_spec.rb
@@ -18,3 +18,11 @@ end
 describe file('/etc/default/grub') do
   its(:content) { should match /GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/}
 end
+
+describe group('docker') do
+  it { should exist }
+end
+
+describe user('ubuntu') do
+  it { should belong_to_group 'docker' }
+end
